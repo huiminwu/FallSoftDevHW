6041827d570ab5e8245043fef2eeb6b1a5f6a211
huiminwu0 <huiminwu24@gmail.com>
2018-09-13 21:58:15 -0400

    Add hw03

---
+++ 03_occupation/getOccupation.py
@@ -0,0 +1,25 @@
+# Team InZane HuiMins - Hui Min Wu and Zane Wang
+# SoftDev1 pd8
+# K06 -- Stl/O: Divine your Destiny!
+# 2018-09-13
+
+import random
+import csv
+
+dict = {}
+with open('occupations.csv') as csvfile:
+    reader = csv.DictReader(csvfile) #using dictreader instead of reader allows us to be able to pass parameters from the 1st row to create dict
+    for row in reader:
+        dict[row['Job Class']] = float(row['Percentage'])    
+    del dict['Total']
+    #testing: print(dict)
+
+def randomOccupation():
+    randomselect = random.uniform(0, 99.8) #returns a random floating point number including 0 and 99.8
+    #testing: print (randomselect)
+    for x in dict:
+        randomselect = randomselect - dict[x]
+        if (randomselect <= 0):
+            return x
+
+print(randomOccupation())
