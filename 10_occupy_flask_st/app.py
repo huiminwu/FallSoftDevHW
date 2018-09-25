d93d452f8273a55f9ab33ab287b2584409008226
huiminwu0 <huiminwu24@gmail.com>
2018-09-25 01:03:59 -0400

    Apply feedback and add comments to code

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -6,17 +6,17 @@
 from flask import Flask, render_template
 import random
 import csv
-import getOccupation
+import getOccupation #imports file from previous homework
 
 app = Flask(__name__)
 
-@app.route("/occupations")
+@app.route("/occupations") #creates route to occupations
 def occupation():
     return render_template('template.html',
                     title = 'Occupations',
                     heading = 'Below shows the percentages of Americans with the corresponding job. You''ll also get a random job generated with the percentages as the probability.',
-                    collection = getOccupation.generateDict(),
-                    text = 'Job: ' + getOccupation.randomOccupation())
+                    collection = getOccupation.generateDict(), #calls the generateDict function in the getOccupation file
+                    text = 'Job: ' + getOccupation.randomOccupation()) #calls the randomOccupation function in the getOccupation file
 
 
 if __name__ == "__main__":
