bdf20d171c165cee1eb10f1261089aa4c213b7ed
huiminthehuman <huiminwu24@gmail.com>
2018-10-02 17:47:58 -0400

    Start hw15

---
+++ 15_flashinh/app.py
@@ -0,0 +1,43 @@
+#Hui Min Wu
+#SoftDev1 pd7
+#K14 -- Do I Know You?
+#2018-10-01
+
+from flask import Flask, render_template, request, url_for, redirect, session
+import os
+app = Flask(__name__)
+app.secret_key = os.urandom(32) #generate a private key and assing to built in secret key
+
+user = {"hui" : "min"}
+
+@app.route("/", methods=["POST", "GET"])
+def display():
+    if "hui" in session:
+        return render_template('welcome.html',
+                               text = "Welcome!")
+    return redirect(url_for("display_login")) #if you're not logged in, you're redirected to the login page
+
+@app.route("/login", methods=["POST", "GET"])
+def display_login():
+    return render_template('login.html')
+
+@app.route("/auth", methods=["POST", "GET"])
+def authorizer():
+    user = request.args['username']
+    passwd = request.args['password']
+    if(user == "hui" and passwd == "min"): #if credentials are right
+        session["hui"] = "min"
+        return render_template('welcome.html',
+                               text = "Welcome!")
+    else: #if credentials arent right
+        return render_template('error.html',
+                               text = "Sorry, try again by going to root route")
+
+@app.route("/logout")
+def logout():
+    session.pop("hui") #remove data from session
+    return redirect(url_for("display_login"))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
