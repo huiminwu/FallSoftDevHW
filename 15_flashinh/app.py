ec82252e6914a6c1ed2271dc2a45ce8a590f515b
huiminwu0 <huiminwu24@gmail.com>
2018-10-03 00:19:50 -0400

    Finish flashing

--- 15_flashinh/app.py
+++ 15_flashinh/app.py
@@ -5,43 +5,34 @@
 
 from flask import Flask, render_template, request, url_for, redirect, session, flash
 import os
+
 app = Flask(__name__)
 app.secret_key = os.urandom(32) #generate a private key and assing to built in secret key
-
-user = {"hui" : "min"}
-
-@app.route("/", methods=["POST", "GET"])
+usr = "hui"
+pwd = "min"
+@app.route("/")
 def display():
-    if "hui" in session:
+    if usr in session:
         return render_template('welcome.html',
                                text = "Welcome!")
-    return redirect(url_for("display_login")) #if you're not logged in, you're redirected to the login page
-
-@app.route("/login", methods=["POST", "GET"])
-def display_login():
     return render_template('login.html')
 
-@app.route("/auth", methods=["POST", "GET"])
+@app.route("/auth", methods=["POST"])
 def authorizer():
     user = request.form['username']
     passwd = request.form['password']
-    if(user == "hui" and passwd == "min"): #if credentials are right
-        session["hui"] = "min"
+    if(user == usr and passwd == pwd): #if credentials are right
+        session[usr] = pwd
+        flash("Correct credentials")
         return render_template('welcome.html',
                                text = "Welcome!")
-    elif(user == "hui"):
-        flash("Wrong password!")
-    elif(passwd != "min"):
-        flash("Wrong password and username!")
-    else:
-        flash("Wrong username!")
-    return render_template('error.html',
-                            text = "Sorry, try again by going to root route")
+    flash("Wrong credentials!")
+    return render_template('login.html')
 
-@app.route("/logout")
+@app.route("/logout", methods=["POST"])
 def logout():
     session.pop("hui") #remove data from session
-    return redirect(url_for("display_login"))
+    return redirect(url_for("display"))
 
 if __name__ == "__main__":
     app.debug = True
