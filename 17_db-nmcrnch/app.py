567d53e68380f277f616f746818d97ad7cf710be
huiminthehuman <huiminwu24@gmail.com>
2018-10-05 14:08:00 -0400

    Add classwork

---
+++ 17_db-nmcrnch/app.py
@@ -0,0 +1,55 @@
+#Hui Min Wu and Maryann Foley Team Lil Peeps
+#SoftDev1 pd8
+#2018-10-05
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitates CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#==========================================================
+#INSERT YOUR POPULATE CODE IN THIS ZONE
+def createStuds():
+    
+    command = "CREATE TABLE peps (name TEXT, age INTEGER, id INTEGER)" #creates table called peps with those parameters
+    c.execute(command) #executes the command above
+    with open('raw/peeps.csv', newline = '') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            c.execute("INSERT INTO peps VALUES (\" {0} \", {1}, {2})".format(row["name"], row["age"], row["id"]))
+
+
+def createCourses():
+
+    command = "CREATE TABLE coarses (code TEXT, mark INTEGER, id INTEGER)"
+    c.execute(command)
+
+    with open('raw/courses.csv', newline = '') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            c.execute("INSERT INTO coarses VALUES (\" {0} \", {1}, {2})".format(row["code"], row["mark"], row["id"]))
+def lookUp():
+    c.execute("SELECT name, peps.id, mark FROM peps, coarses WHERE peps.id = coarses.id")
+    print(c.fetchall())
+
+def average():
+    for i in range(1,11):
+        c.execute("SELECT mark FROM coarses WHERE coarses.id = " + i)
+        sumAvgs = c.fetchall()
+        print(sumAvgs)
+
+
+createStuds()
+createCourses()
+lookUp()
+average()
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
+
+
