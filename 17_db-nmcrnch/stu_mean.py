77867efb1594815fb2383670a80d8040d3ec0265
huiminwu0 <huiminwu24@gmail.com>
2018-10-06 23:00:20 -0400

    Finish hw17

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -37,20 +37,58 @@ def lookUp():
     c.execute("SELECT name, peps.id, mark FROM peps, coarses WHERE peps.id = coarses.id")
     print(c.fetchall())
 
-def average():
+def average(printTF=False):
+    retList=[]
     for i in range(1,11):
-        c.execute("SELECT mark FROM coarses WHERE coarses.id = " + i)
-        sumAvgs = c.fetchall()
-        print(sumAvgs)
+        total=0
+        numOfMarks=0
+        name=""
+        c.execute("SELECT mark,name FROM coarses,peps WHERE coarses.id = peps.id AND peps.id = "+ str(i))
+        for row in c:
+            total+=row[0]
+            numOfMarks+=1
+            name=row[1]
+        avg=total/numOfMarks
+        if printTF:
+            print(name,i,avg)
+        retList.append([i,avg])
+    return retList
 
+def createAverage():
+    c.execute("CREATE TABLE peeps_avg (id INTEGER,avg NUMBER)")
+    avgs=average(True)
+    for avg in avgs:
+        c.execute("INSERT INTO peeps_avg VALUES (?,?)",avg)
+
+def newCourse(course,mark,id):
+    c.execute("INSERT INTO coarses VALUES (\" {0} \", {1}, {2})".format(course, mark, id))
+    recalculateAvgs()
+
+def recalculateAvgs():
+    for i in range(1,11):
+        c.execute("DELETE FROM peeps_avg WHERE peeps_avg.id = "+str(i))
+    avgs=average()
+    for avg in avgs:
+        c.execute("INSERT INTO peeps_avg VALUES (?,?)",avg)
 
 createStuds()
 createCourses()
 lookUp()
-average()
+print("==================OLD AVERAGES================")
+createAverage()
+newCourse("wood",71,9)
+newCourse("woof",0,1)
+newCourse("woog",7,2)
+newCourse("woop",900,3)
+newCourse("woow",78,4)
+newCourse("woom",694,5)
+newCourse("woor",666,6)
+newCourse("wooa",21,7)
+newCourse("wooo",8,8)
+newCourse("wooe",777777,10)
+print("=================NEW AVERAGES=================")
+average(True)
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
-
