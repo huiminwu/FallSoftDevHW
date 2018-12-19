06722fe71ca45662d93e60f80237b52d53281843
huiminwu0 <huiminwu24@gmail.com>
2018-12-18 20:47:39 -0500

    Finish hw

--- 28_js/funky_town.js
+++ 28_js/funky_town.js
@@ -1,3 +1,8 @@
+//MinZane -- Hui Min Wu, Zane Wang
+//SoftDev1 pd8
+//K28 -- Sequential Progression
+//2018-12-18
+
 var fibonacci = function(n) {
     if (n == 0) {
         return 0;
@@ -5,11 +10,23 @@ var fibonacci = function(n) {
         return 1;
     } else {
         return fibonacci(n-2) + fibonacci(n-1);
-    }
-}
+    };
+};
 
 var gcd = function(a, b) {
     if (a < b) {
-        return function(b,a);
+        return gcd(b, a);
+    } else if (b == 0) {
+        return a;
     } else if (a == 0) {
-        return a
+        return 0;
+    } else {
+        return gcd(b, a % b);
+    };
+};
+
+var arrayName = ["Hui Min", "Zane", "Johnny"];
+
+var randomStudent = function() {
+    return arrayName[Math.floor(Math.random() * arrayName.length)];
+}
