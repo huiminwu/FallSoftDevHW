fe6936fcd21988c641e8ede0fb70a709797ac61f
huiminthehuman <huiminwu24@gmail.com>
2018-12-19 13:24:21 -0500

    class

---
+++ 29_js/funky_town.js
@@ -0,0 +1,47 @@
+//MinZane -- Hui Min Wu, Zane Wang
+//SoftDev1 pd8
+//K28 -- Sequential Progression
+//2018-12-18
+
+var fibonacci = function(n) {
+    if (n == 0) {
+        return 0;
+    } else if (n <= 2) {
+        return 1;
+    } else {
+        return fibonacci(n-2) + fibonacci(n-1);
+    };
+};
+
+var gcd = function(a, b) {
+    if (a < b) {
+        return gcd(b, a);
+    } else if (b == 0) {
+        return a;
+    } else if (a == 0) {
+        return 0;
+    } else {
+        return gcd(b, a % b);
+    };
+};
+
+var arrayName = ["Hui Min", "Zane", "Johnny"];
+
+var randomStudent = function() {
+    return arrayName[Math.floor(Math.random() * arrayName.length)];
+}
+
+var a = document.getElementById("b");
+a.addEventListener("click", function() {
+    console.log(fibonacci(5))
+});
+
+var b = document.getElementById("b");
+b.addEventListener("click", function() {
+    console.log(fibonacci(5))
+});
+
+var c = document.getElementById("b");
+c.addEventListener("click", function() {
+    console.log(fibonacci(5))
+});
